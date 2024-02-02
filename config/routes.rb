9a9ddf6e366baf2064ea96e7ca98a1b518993709
ToshitakaIwatani <144602782+IwataniToshitaka@users.noreply.github.com>
2024-02-02 05:43:40 +0000

    [Add]Log in

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'top', to: 'homes#top'
+  get 'about', to: 'homes#about'
+  #home/aboutのURLに飛んだ時、homesコントローラのindexアクションを実行
+
+  resources :user, only: [:new, :create, :show, :index, :edit]
+  resources :airticle, only: [:new, :create, :show, :index]
+  resources :airticle_genre, only: [:new, :create, :index, :show]
+  resources :genre, only: [:index]
+  resources :comment, only: [:new, :edit, :show]
+  resources :like, onlu: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
